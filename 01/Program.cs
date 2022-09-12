da4ce518be22cd60ced98916d9875a31cfa2998c
unknown <ansuzpert88@gmail.com>
2022-09-12 19:09:01 +0300

    Задания

---
+++ 01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("ВВедите количество вводимых чисел");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите числа");
+int x;
+int kol=0;
+for (int i=0; i<M; i++)
+{
+    x = Convert.ToInt32(Console.ReadLine());
+    if (x>0) kol++;
+}
+Console.WriteLine($"Чисел больше нуля {kol}");
